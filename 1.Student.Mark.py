9e17ecd2cfeefc90fe2d8ee2da0d080959c23946
BI12-023_Nguyen_Nhat_Anh <anhnn.bi12-023@st.usth.edu.vn>
2023-02-21 14:59:53 +0700

    My another way to do the exercise 1

--- 1.Student.Mark.py
+++ 1.Student.Mark.py
@@ -1,40 +1,30 @@
+def insert_student(n, student):
+    for i in range(n):
+        print("Input in the right formular ID,Name,Dob: ",end='')
+        k=map(str, input().split(","))
+        student.append(list(k))
+
+def insert_subject(m,subject):
+    for i in range(m):
+        k=map(str,input("Input the right formula ID,name course: ").split(","))
+        subject.append(list(k))
+
+def insert_mark(n,stduent,subject,mark):
+    for i in student:
+        point=[]
+        for j in subject:
+            k=float(input(j[1]+" score of student "+i[1]+": "))
+            point.append(k)
+        mark.append(point)
+
 n=int(input("Number of student: "))
-sinhvien_name=[]
-sinhvien_id=[]
-sinhvien_dob=[]
-for i in range(n):
-    k=input("ID of student "+str(i+1)+": ")
-    sinhvien_id.append(k)
-    k=input("Name of student "+str(i+1)+": ")
-    sinhvien_name.append(k)
-    k=input("Date of birth of student "+str(i+1)+": ")
-    sinhvien_dob.append(k)
-
-m=int(input("Number of course: "))
-course_id=[]
-course_name=[]
-for i in range(m):
-    k=input("ID of course "+str(i+1)+": ")
-    course_id.append(k)
-    k=input("Name of course: ")
-    course_name.append(k)
-
-diem=[]
-for i in range(m):
-    for j in range(n):
-        k=float(input(course_name[i]+" score of "+sinhvien_name[j]+": "))
-        diem.append(k)
-
-cnt=0
-
-print("\n")
-print("List of student:")
-for i in range(n):
-    print(sinhvien_id[i],sinhvien_name[i],sinhvien_dob[i])
-
-for i in range(m):
-    print("\nID course:",course_id[i])
-    print("Name course:",course_name[i])
-    for j in range(n):
-        print(sinhvien_id[j],sinhvien_name[j]+": ",diem[cnt])
-        cnt+=1        
+student=[]
+insert_student(n,student)
+subject=[]*2
+m=int(input("Number of object: "))
+insert_subject(m,subject)
+mark=[]
+insert_mark(n,student,subject,mark)
+print(student)
+print(subject)
+print(mark)
