20034861453a836ade3ee8ea9f2182589a510f00
BI12-023-Nguyen-Nhat-Anh <anhnn.bi12-023@st.usth.edu.vn>
2023-02-24 02:49:19 +0700

    I put some comment and I added polymorphism

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,13 +1,19 @@
+#Class person has all the basic human information
 class person:
-    def __init__(self,id,name,dob):
-        self.id=id
+    def __init__(self,name,dob):
         self.name=name
         self.dob=dob
 
+#Class student inherits the name and date of birth attributes from the person and gets the id value
 class student(person):
-    def show(self):
-        return self.id+","+self.name+","+self.dob
+    def __init__(self,id,name,dob):
+        super().__init__(name,dob)
+        self.id=id
 
+    def show(self):
+        return self.id,self.name,self.dob
+
+#Class subject has function to get id and name of subject
 class subject:
     __id: str
     __name: str
@@ -17,14 +23,15 @@ class subject:
 
     def get_info(self):
         return self.__id, self.__name
-    
 
+#Display function makes the output easier to see
 def display(arr1,arr2,arr3):
     for i in range(len(arr1)):
         print("\n"*2)
         print(arr1[i][0]+" "+arr1[i][1]+" "+arr1[i][2]+": ")
+        kc=len(arr1[i][0])+len(arr1[i][1])+len(arr1[i])
         for j in range(len(arr2)):
-            print("                       "+arr2[j][0]+" "+arr2[j][1]+": ",end='')
+            print(" "*(kc+3)+arr2[j][0]+" "+arr2[j][1]+": ",end='')
             print(arr3[i][j])
 
 n=int(input("Number of student: "))
@@ -34,8 +41,7 @@ for i in range(n):
     name=input("Student's name: ")
     dob=input("Student's date of birth: ")
     a=student(id,name,dob)
-    k=map(str,a.show().split(","))
-    arr1.append(list(k))
+    arr1.append(a.show())
 
 m=int(input("Number of subject: "))
 arr2=[]
